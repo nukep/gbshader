58eceec3f281bf492dccf5e57de24d982146e829
Danny Spencer <dan.spencer.np@gmail.com>
2026-02-06 13:20:59 -0800

    Update README to show support for RGBDS 1.0.1, make build show more warnings

--- README.md
+++ README.md
@@ -14,7 +14,7 @@ To compile the demos, run `./compile_gbspin.sh` or `./compile_teapot.sh`.
 
 Ensure you have the RGBDS toolkit installed and on your PATH: https://rgbds.gbdev.io/
 
-RGBDS v0.9.1 is the version used for this project. Later versions may or may not work.
+RGBDS v1.0.1 is the version used for this project. Earlier/later versions may or may not work.
 
 The normal map conversion is a Python script, which has some dependencies. Install the dependencies using pip:
 
--- src/build.sh
+++ src/build.sh
@@ -6,10 +6,12 @@ mkdir -p build
 
 rgbgfx hud.png -c '#000,#555,#aaa,#fff' -u -o build/hud_tiledata.bin -t build/hud_tilemap.bin -a build/hud_tileattr.bin
 
-rgbasm main.asm -o build/main.o
-rgbasm lookup_tables.asm -o build/lookup_tables.o
-rgbasm shader.asm -o build/shader.o
-rgbasm image.asm -o build/image.o
+ASMFLAGS="-Wextra"
+
+rgbasm $ASMFLAGS main.asm -o build/main.o
+rgbasm $ASMFLAGS lookup_tables.asm -o build/lookup_tables.o
+rgbasm $ASMFLAGS shader.asm -o build/shader.o
+rgbasm $ASMFLAGS image.asm -o build/image.o
 rgblink -w build/*.o -l layout.link -o build/main.gb -m build/main.map -n build/main.sym
 # GBC only
 rgbfix -C -v -m mbc3 build/main.gb
